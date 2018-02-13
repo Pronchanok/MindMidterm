334bff451d7d69d311c772674e6bf288dd3d08b8
Pronchanok <zd1003.zd@gmail.com>
2018-02-13 15:32:27 +0700

    Give up T_T 15:31

--- app/src/main/java/pronchanok/krirk/ac/th/mindmidterm/CalculateFragment.java
+++ app/src/main/java/pronchanok/krirk/ac/th/mindmidterm/CalculateFragment.java
@@ -3,9 +3,11 @@ package pronchanok.krirk.ac.th.mindmidterm;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by HP on 13/2/2561.
@@ -17,5 +19,40 @@ public class CalculateFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calculate, container, false);
         return view;
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button button = getView().findViewById(R.id.btnExchange);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        Toolbar toolbar = getView().findViewById(R.id.ToolBarCalculate);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate USD to THB");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
     }
 }
